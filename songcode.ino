eeeab200f2a780cdac94acde1291f0701a18ad1a
czaragosa <cyanazaragosa35@gmail.com>
2023-04-17 13:30:04 -0500

    notes

--- songcode.ino
+++ songcode.ino
@@ -1,6 +1,17 @@
 void setup() {
   // Cyana code
-  int C4 = 262
+  int C4 = 262;
+int halfnote = 900/2;
+int output = 13;
+int D4 = 294;
+int E4 = 330;
+int F4 = 349;
+int G4 = 392;
+int dotquarter = 900/2;
+int quarter = 667/2;
+int sixteenth = 333/2;
+int dothalf = 2000/2;
+
   
   // Randee code
   int D4 = 455
