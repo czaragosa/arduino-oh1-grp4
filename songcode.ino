c25c6d32eefb9eca92a0d3b8ea657d48623672f1
randeeh <randeerae1212@gmail.com>
2023-04-17 13:33:00 -0500

    test1

--- songcode.ino
+++ songcode.ino
@@ -15,7 +15,16 @@ int dothalf = 2000/2;
   
   // Randee code
   int D4 = 467
-
+int halfnote = 900/2;
+int output = 13;
+int D4 = 294;
+int E4 = 330;
+int F4 = 349;
+int G4 = 392;
+int dotquarter = 900/2;
+int quarter = 667/2;
+int sixteenth = 333/2;
+int dothalf = 2000/2;
   // Felix code
   //code change
 
