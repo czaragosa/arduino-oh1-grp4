893f28278a842d98a9ce96b4df6bccfe26cf4677
randeeh <randeerae1212@gmail.com>
2023-04-24 13:51:56 -0500

    test commit

--- songcode.ino
+++ songcode.ino
@@ -9,23 +9,14 @@ int F4 = 349;
 int G4 = 392;
 int eigth = 666;
 int quarter = 667/;
-int sixteenth = 333;
+int sixteenth = 333; 
 int whole = 1800;
 
   
   // Randee code
+  //slay queen
   int D4 = 467
-int halfnote = 900/2;
-int output = 13;
-int D4 = 294;
-int E4 = 330;
-int F4 = 349;
-int G4 = 392;
-int dotquarter = 900/2;
-int quarter = 667/2;
-int sixteenth = 333/2;
-int dothalf = 2000/2
-slay queen;
+
   // Felix code
   //code changes, more changes
 
