fe02368f232bbf8aa797fb8ac9cf2ee53ba9ef69
czaragosa <cyanazaragosa35@gmail.com>
2023-04-24 13:43:16 -0500

    cyana code

--- songcode.ino
+++ songcode.ino
@@ -1,4 +1,27 @@
 // cyana code
+/*
+  Thousand Years by Christina Perri
+*/
+
+  #define Note_G4 392
+  #define Note_B4 494
+  #define Note_A4 440
+  #define Note_D5 587
+  #define Note_C5 523
+  #define Note_FS4 370
+  #define Note_F4 349
+  #define Rest    0
+
+
+  // can change speed of song
+  int tempo = 170;
+
+  //change to whichever pin being used
+  int buzzer = 13;
+
+  // notes of the melody followed by the duration.
+  // 4 means quarter note, 2 means half note...
+  // negative numbers represent dotted notes
 // randee code
 // natalie code
 // felix code
