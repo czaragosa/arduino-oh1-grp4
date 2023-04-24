4c5c92fb6adc1d9d9aa00b978a19e8a7e4cc6167
czaragosa <cyanazaragosa35@gmail.com>
2023-04-24 13:27:13 -0500

    deleted random notes

--- songcode.ino
+++ songcode.ino
@@ -1,17 +1,5 @@
 void setup() {
   // Cyana code
-  int C4 = 262;
-int halfnote = 900;
-int output = 13;
-int D4 = 294;
-int E4 = 330;
-int F4 = 349;
-int G4 = 392;
-int eigth = 666;
-int quarter = 667/;
-int sixteenth = 333;
-int whole = 1800;
-
   
   // Randee code
   int D4 = 467
