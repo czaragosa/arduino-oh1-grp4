d65b31cfe77e7f57a4617bc946128c1d61725786
nataliepulido24 <natalie.pulido@icloud.com>
2023-04-26 13:28:53 -0500

    natalie rest code

--- songcode.ino
+++ songcode.ino
@@ -73,7 +73,11 @@ void setup() {
 
 
 void loop() {
+// iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
 
+    // calculates the duration of each note
 }
 
 
