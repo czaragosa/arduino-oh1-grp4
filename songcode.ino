723d094f03ceb8543f14fd73e42d61f0e8638006
fsancar3 <felix.sanchez-carrera@my.utsa.edu>
2023-05-01 11:27:07 -0500

    felix code

--- songcode.ino
+++ songcode.ino
@@ -49,3 +49,57 @@
     Note_A3,2, Note_C4,4, //12
     Note_C4,2,
     Note_C4,2,
+
+     // Felix Code
+    Note_E4,-4, Note_D4,-4, //thou-sand
+    Note_C4,-2, //more.
+    Rest,-4, Note_E4,-4,
+    Note_D4,-4, Note_C4,-4,
+    Note_B3,-4, Note_F4,-4,
+    Note_E4,-4, Note_D4,-4,
+    Note_B3,-4, Note_E4,-4,
+    Note_D4,-4, Note_C4,-4,
+    Note_B3,-4, Note_D4,-4,
+    Note_C4,-4, Note_A3,-4,
+    Note_B3,-2,
+
+  } ;
+
+  // sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+  // there are two values per note (pitch and duration), so for each note there are four bytes
+  int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+  // this calculates duration of wholenote in ms
+  int wholenote = (60000 * 4) / tempo;
+
+  int divider = 0, noteDuration = 0;
+
+void setup() {
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regula note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted note are represented with negative durations
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }  
+    //w we only play note for 90% of duration, leaving 10% as pause
+    tone(buzzer, melody[thisNote], noteDuration*0.9);
+
+    // wait for the specific duration before playing note
+    delay(noteDuration);
+
+    // stop the waveform generation before next note
+    noTone(buzzer);  
+    }
+  }
+
+void loop() {
+  // no need to repeat the melody
+}
